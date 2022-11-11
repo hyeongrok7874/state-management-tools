fa45af966c15bb1ab98cac7e1c62bf255f676187
hyeongrok7874 <khr5k0228@gmail.com>
2022-11-11 09:34:26 +0900

    Add CountText

---
+++ packages/recoil/src/CountText.tsx
@@ -0,0 +1,11 @@
+import { useRecoilState } from "recoil";
+import countAtom from "./countAtom";
+import { Text } from "@chakra-ui/react";
+
+const CountText = () => {
+  const [count, _] = useRecoilState(countAtom);
+
+  return <Text fontSize="200px">{count}</Text>;
+};
+
+export default CountText;
