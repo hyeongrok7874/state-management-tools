d83acb6ef396a0fc1923f80d93d8e2a608c9c3a9
hyeongrok7874 <khr5k0228@gmail.com>
2022-11-11 12:14:39 +0900

    Add Provider

--- packages/mobx/src/App.tsx
+++ packages/mobx/src/App.tsx
@@ -1,23 +1,28 @@
 import React from "react";
+import { ChakraProvider, Flex, Heading } from "@chakra-ui/react";
+import { Provider } from "mobx-react";
+import CountText from "./CountText";
+import Controller from "./Controller";
+import CountStore from "./CountStore";
 
 function App() {
+  const counter = new CountStore();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Provider counter={counter}>
+      <ChakraProvider>
+        <Flex
+          height="100vh"
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Heading fontSize="100px">MobX</Heading>
+          <CountText />
+          <Controller />
+        </Flex>
+      </ChakraProvider>
+    </Provider>
   );
 }
 
