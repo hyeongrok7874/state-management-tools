60c2a70824308280895569e82357aa9c875ed95b
hyeongrok7874 <khr5k0228@gmail.com>
2022-11-11 12:15:03 +0900

    Add useCountStore

---
+++ packages/mobx/src/Controller.tsx
@@ -0,0 +1,20 @@
+import { Button, Flex } from "@chakra-ui/react";
+import useStore from "./useStore";
+import { observer, inject } from "mobx-react";
+
+export default observer(function Controller() {
+  const {
+    counter: { decrease, increase },
+  } = useStore();
+
+  return (
+    <Flex width="200px" justifyContent="space-between">
+      <Button size="lg" fontSize="30px" colorScheme="red" onClick={decrease}>
+        -1
+      </Button>
+      <Button size="lg" fontSize="30px" colorScheme="blue" onClick={increase}>
+        +1
+      </Button>
+    </Flex>
+  );
+});
---
+++ packages/mobx/src/CountText.tsx
@@ -0,0 +1,13 @@
+import { Text } from "@chakra-ui/react";
+import { observer } from "mobx-react";
+import useStore from "./useStore";
+
+const CountText = observer(() => {
+  const {
+    counter: { count },
+  } = useStore();
+
+  return <Text fontSize="200px">{count}</Text>;
+});
+
+export default CountText;
