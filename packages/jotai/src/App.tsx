605e3e6c13de7a4bbeeba15e1932899b4e833d09
hyeongrok7874 <khr5k0228@gmail.com>
2022-11-12 17:23:06 +0900

    Add components in app

--- packages/jotai/src/App.tsx
+++ packages/jotai/src/App.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 import { Provider } from "jotai";
+import { ChakraProvider, Flex, Heading } from "@chakra-ui/react";
+import { Controller, CountText } from "components";
 
 function App() {
-  return <Provider></Provider>;
+  return (
+    <ChakraProvider>
+      <Flex
+        height="100vh"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+      >
+        <Heading fontSize="100px">Jotai</Heading>
+        <CountText />
+        <Controller />
+      </Flex>
+    </ChakraProvider>
+  );
 }
 
 export default App;
