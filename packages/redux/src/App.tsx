40cdcae1839b5ab1aeaab793904fbaf49e868126
hyeongrok7874 <khr5k0228@gmail.com>
2022-11-14 14:47:11 +0900

    Add components in app

--- packages/redux/src/App.tsx
+++ packages/redux/src/App.tsx
@@ -1,8 +1,28 @@
-import React from "react";
-import "./App.css";
+import { ChakraProvider, Flex, Heading } from "@chakra-ui/react";
+import { Controller, CountText } from "components";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./reducer";
+
+const store = createStore(rootReducer);
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Provider store={store}>
+      <ChakraProvider>
+        <Flex
+          height="100vh"
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
+        >
+          <Heading fontSize="100px">Redux</Heading>
+          <CountText />
+          <Controller />
+        </Flex>
+      </ChakraProvider>
+    </Provider>
+  );
 }
 
 export default App;
